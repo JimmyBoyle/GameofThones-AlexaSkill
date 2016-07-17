0657dcf450f5f6c31f5038390e3759e9581532b5
Jimmy Boyle <jimmy@theboylefamily.net>
2016-07-16 18:11:57 -0700

    V 1.0 -- added Character houses
    Ask Alexa for the house of a character and she will read it back to you

--- src/intents/characterHouse.js
+++ src/intents/characterHouse.js
@@ -1,13 +1,46 @@
+var AlexaSkill = require('../AlexaSkill');
 var getThronesData = require('../utilities/thronesApi');
+var helper = require('../utilities/helper');
 
 var characterHouse = function(intent, session, response) {
 
-    function test() {
-        getThronesData.getCharacterByName("Jon Snow", function thronesCallback(thronesResponse) {
-            console.log(thronesResponse[0]);
+    function setup() {
+        var charName = intent.slots.CharacterName;
+        if (charName && charName.value) {
+            charName = helper.toTitleCase(String(charName.value));
+            getCharacterHouse(charName);
+        } else {
+            response.ask("I'm sorry i didnt understand that name please repeat your phrase", "Repeat");
+        }
+    }
+
+    function getCharacterHouse(charName) {
+        var i = 0;
+        if (charName === "Daenerys Targaryen") {
+            i = 1;
+        }
+        getThronesData.getCharacterByName(charName, function nameCallback(nameResponse) {
+            var houses = nameResponse[i].allegiances;
+            if (houses.length == 0) {
+                response.tell("I'm sorry, I couldn't find any house for " + charName + ". There are just so many in 'A Song of Ice and Fire'.");
+            } else {
+                var houseId = houses[i].substring(44);
+                getHouseName(houseId, charName);
+            }
         });
     }
-    test();
+
+    function getHouseName(houseID, charName) {
+        getThronesData.getHouseById(houseID, function houseCallback(houseResponse) {
+            var houseName = houseResponse.name;
+            var speechOutput = {
+                speech: charName + " is of " + houseName,
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+            };
+            response.tellWithCard(speechOutput, charName, houseName);
+        });
+    }
+    setup();
 };
 
 exports.register = characterHouse;
